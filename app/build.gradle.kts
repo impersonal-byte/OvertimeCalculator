f75f81d372cd3cb8456881b99d00bf99623cb755
impersonal-byte <73073155+impersonal-byte@users.noreply.github.com>
2026-03-21 18:39:28 +0800

    release: bump version to 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 
-val appVersionCode = 16
-val appVersionName = "2.2.0"
+val appVersionCode = 17
+val appVersionName = "2.3.0"
 val localProperties = Properties().apply {
     val localPropertiesFile = rootProject.file("local.properties")
     if (localPropertiesFile.exists()) {
